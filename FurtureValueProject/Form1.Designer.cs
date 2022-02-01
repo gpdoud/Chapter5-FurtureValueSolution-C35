b3177a4fb1263ae04a0759135ecce97efa1c6e1c
Greg Doud <gdoud@gmail.com>
2022-01-31 20:46:32 -0500

    Changes for Chapter 6

--- FurtureValueProject/Form1.Designer.cs
+++ FurtureValueProject/Form1.Designer.cs
@@ -38,74 +38,82 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(32, 25);
+            this.label1.Location = new System.Drawing.Point(37, 33);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(117, 15);
+            this.label1.Size = new System.Drawing.Size(142, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Monthly Investment:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(32, 63);
+            this.label2.Location = new System.Drawing.Point(37, 84);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(109, 15);
+            this.label2.Size = new System.Drawing.Size(138, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "Yearly Interest Rate:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(32, 101);
+            this.label3.Location = new System.Drawing.Point(37, 135);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(98, 15);
+            this.label3.Size = new System.Drawing.Size(122, 20);
             this.label3.TabIndex = 2;
             this.label3.Text = "Number of Years:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(32, 139);
+            this.label4.Location = new System.Drawing.Point(37, 185);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(75, 15);
+            this.label4.Size = new System.Drawing.Size(93, 20);
             this.label4.TabIndex = 3;
             this.label4.Text = "Future Value:";
             // 
             // txtMonthlyInvestment
             // 
-            this.txtMonthlyInvestment.Location = new System.Drawing.Point(170, 22);
+            this.txtMonthlyInvestment.Location = new System.Drawing.Point(194, 29);
+            this.txtMonthlyInvestment.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtMonthlyInvestment.Name = "txtMonthlyInvestment";
-            this.txtMonthlyInvestment.Size = new System.Drawing.Size(100, 23);
+            this.txtMonthlyInvestment.Size = new System.Drawing.Size(114, 27);
             this.txtMonthlyInvestment.TabIndex = 4;
+            this.txtMonthlyInvestment.TextChanged += new System.EventHandler(this.ClearFutureValue);
             // 
             // txtInterestRate
             // 
-            this.txtInterestRate.Location = new System.Drawing.Point(170, 60);
+            this.txtInterestRate.Location = new System.Drawing.Point(194, 80);
+            this.txtInterestRate.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtInterestRate.Name = "txtInterestRate";
-            this.txtInterestRate.Size = new System.Drawing.Size(100, 23);
+            this.txtInterestRate.Size = new System.Drawing.Size(114, 27);
             this.txtInterestRate.TabIndex = 5;
+            this.txtInterestRate.TextChanged += new System.EventHandler(this.ClearFutureValue);
             // 
             // txtYears
             // 
-            this.txtYears.Location = new System.Drawing.Point(170, 98);
+            this.txtYears.Location = new System.Drawing.Point(194, 131);
+            this.txtYears.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtYears.Name = "txtYears";
-            this.txtYears.Size = new System.Drawing.Size(100, 23);
+            this.txtYears.Size = new System.Drawing.Size(114, 27);
             this.txtYears.TabIndex = 6;
+            this.txtYears.TextChanged += new System.EventHandler(this.ClearFutureValue);
             // 
             // txtFutureValue
             // 
-            this.txtFutureValue.Location = new System.Drawing.Point(170, 136);
+            this.txtFutureValue.Location = new System.Drawing.Point(194, 181);
+            this.txtFutureValue.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtFutureValue.Name = "txtFutureValue";
             this.txtFutureValue.ReadOnly = true;
-            this.txtFutureValue.Size = new System.Drawing.Size(100, 23);
+            this.txtFutureValue.Size = new System.Drawing.Size(114, 27);
             this.txtFutureValue.TabIndex = 7;
             this.txtFutureValue.TabStop = false;
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(170, 192);
+            this.btnExit.Location = new System.Drawing.Point(194, 256);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.Size = new System.Drawing.Size(86, 31);
             this.btnExit.TabIndex = 8;
             this.btnExit.Text = "E&xit";
             this.btnExit.UseVisualStyleBackColor = true;
@@ -113,9 +121,10 @@
             // 
             // btnCalculate
             // 
-            this.btnCalculate.Location = new System.Drawing.Point(74, 192);
+            this.btnCalculate.Location = new System.Drawing.Point(85, 256);
+            this.btnCalculate.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnCalculate.Name = "btnCalculate";
-            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.Size = new System.Drawing.Size(86, 31);
             this.btnCalculate.TabIndex = 9;
             this.btnCalculate.Text = "&Calculate";
             this.btnCalculate.UseVisualStyleBackColor = true;
@@ -124,10 +133,10 @@
             // Form1
             // 
             this.AcceptButton = this.btnCalculate;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnExit;
-            this.ClientSize = new System.Drawing.Size(307, 242);
+            this.ClientSize = new System.Drawing.Size(351, 323);
             this.Controls.Add(this.btnCalculate);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.txtFutureValue);
@@ -138,6 +147,7 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Future Value";
--- FurtureValueProject/Form1.cs
+++ FurtureValueProject/Form1.cs
@@ -25,15 +25,23 @@ namespace FurtureValueProject {
             int months = years * 12;
             decimal monthlyInterestRate = yearlyInterestRate / 12 / 100;
 
+            decimal futureValue = CalculateFutureValue(monthlyInvestment, monthlyInterestRate, months);
+
+            txtFutureValue.Text = futureValue.ToString("c");
+            txtMonthlyInvestment.Focus();
+        }
+
+        private decimal CalculateFutureValue(decimal monthlyInvestment, decimal monthlyInterestRate, int months) {
             decimal futureValue = 0m;
             for(int i = 0; i < months; i++) {
 
                 futureValue = (futureValue + monthlyInvestment) * (1 + monthlyInterestRate);
-
             }
+            return futureValue;
+        }
 
-            txtFutureValue.Text = futureValue.ToString("c");
-            txtMonthlyInvestment.Focus();
+        private void ClearFutureValue(object sender, EventArgs e) {
+            txtFutureValue.Text = "";
         }
     }
 }
